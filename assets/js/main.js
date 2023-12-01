1b960c32ae80012b3b9dccab2cda7ca6f997a721
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-12-01 16:14:38 -0300

    feat - initial dinamic content to pokemon list

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,35 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function(response) {
-    console.log(response);
-})
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">posion</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemons = document.getElementById('pokemons');
+
+fetch(url)
+    .then((response) => response.json())
+    .then((body) => body.results)
+    .then((pokemonList) => {
+        for(let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            pokemons.innerHTML += convertPokemonToLi(pokemon);
+        }
+        
+    })
+    .catch((error) => console.log(error));
 
 
