b606f0791f3afeda8595452ebb1f79315a3b56df
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-12-03 15:09:27 -0300

    feat - multiple requests in parallel

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,19 @@
 const pokeApi = {};
 
-pokeApi.getPokemons = async (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
         .then((body) => body.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonDetails) => pokemonDetails)
         .catch((error) => console.log(error));
 }
+
+
 
