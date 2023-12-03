f872073b34a1a9b97163ff2283caf346ef5df5bb
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-12-03 15:32:00 -0300

    feat - pokemon model

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,24 @@
 const pokeApi = {};
 
+function covertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.name = pokeDetail.name;
+    pokemon.number = pokeDetail.order;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+
+    pokemon.types = types;
+    pokemon.type = type;
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(covertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = async (offset = 0, limit = 5) => {
